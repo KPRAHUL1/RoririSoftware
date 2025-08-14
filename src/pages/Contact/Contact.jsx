d576862942794b7e3a33238501f3bdeb6cdcbd60
Manoj7505 <k.mano75005@gmail.com>
2025-08-14 10:59:06 +0530

    commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Handshake, Headset, HelpCircle } from 'lucide-react';
 import ContactUsPage from './components/ContactUsPage';
-import {motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { maillottie } from '../../assets/lottiefiles/lettie';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Contact = () => {
-     const enquiryTypes = [
+  const enquiryTypes = [
     {
       icon: <Handshake size={48} strokeWidth={1.5} />,
       title: 'Partner Enquiry',
@@ -36,111 +36,112 @@ const Contact = () => {
     },
   ];
   return (
-  <>
-  <div>
-         <section className="bg-gradient-to-br  flex items-center justify-center px-6">
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center gap-8 md:gap-8">
-          <motion.div
-          className="flex-1 flex justify-center"
-          initial={{ opacity: 0, x: 50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-        > 
-        {/* className="w-[250px] sm:w-[350px] md:w-[450px] h-[250px] sm:h-[350px] md:h-[450px]" */}
-          <DotLottieReact
-          className="w-[250px] sm:w-[350px] md:w-[450px] h-[250px] sm:h-[350px] md:h-[450px]"
-          src={maillottie}
-          loop
-          autoplay
-        />
-        </motion.div>
-        
-        <motion.div
-          className="flex-1 text-center md:text-left mt-10"
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8 }}
-        >
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-medium drop-shadow-lg leading-tight">
-            What can we help you with?
-          </h1>
-          <p className="mt-4 sm:mt-6 text-base sm:text-lgmax-w-lg">
-            Reach out to explore endless possibilities with{" "}
-            <span className="text-blue-400 font-semibold">
-              Roriri Software Solutions!
-            </span>
-          </p>
-
-          <motion.button
-            className="mt-6 px-6 py-3 cursor-pointer bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:bg-blue-300 transition"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Get in Touch
-          </motion.button>
-        </motion.div>
-
-       
-      
-      </div>
-    </section>
-<div className=" top-7/12 left-25  flex items-center justify-center  px-4 sm:px-6  font-inter">
-      <div className="max-w-7xl mx-auto w-full bg-white rounded-3xl  p-8 sm:p-12 relative overflow-hidden">
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {enquiryTypes.map((item, index) => (
-            <div
-              key={index}
-              className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center text-center border border-gray-100 hover:shadow-lg transition-shadow duration-300"
-            >
-              <div className={`p-4 rounded-full ${item.iconBgColor} ${item.iconTextColor} mb-4`}>
-                {item.icon}
-              </div>
-              <h3 className="text-xl font-bold text-gray-900 mb-2">
-                {item.title}
-              </h3>
-              <p className="text-gray-600 text-base mb-6 flex-grow">
-                {item.description}
-              </p>
-              <a
-                href={item.buttonLink}
-                className="inline-block bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold py-2.5 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+    <>
+      <div id="contactpage">
+        <div>
+          <section className="bg-gradient-to-br  flex items-center justify-center px-6">
+            <div className="container mx-auto flex flex-col-reverse md:flex-row items-center gap-8 md:gap-8">
+              <motion.div
+                className="flex-1 flex justify-center"
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8, delay: 0.2 }}
               >
-                {item.buttonText}
-              </a>
+                {/* className="w-[250px] sm:w-[350px] md:w-[450px] h-[250px] sm:h-[350px] md:h-[450px]" */}
+                <DotLottieReact
+                  className="w-[250px] sm:w-[350px] md:w-[450px] h-[250px] sm:h-[350px] md:h-[450px]"
+                  src={maillottie}
+                  loop
+                  autoplay
+                />
+              </motion.div>
+
+              <motion.div
+                className="flex-1 text-center md:text-left mt-10"
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8 }}
+              >
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-medium drop-shadow-lg leading-tight">
+                  What can we help you with?
+                </h1>
+                <p className="mt-4 sm:mt-6 text-base sm:text-lgmax-w-lg">
+                  Reach out to explore endless possibilities with{" "}
+                  <span className="text-blue-400 font-semibold">
+                    Roriri Software Solutions!
+                  </span>
+                </p>
+
+                <motion.button
+                  className="mt-6 px-6 py-3 cursor-pointer bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold rounded-full shadow-lg hover:bg-blue-300 transition"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  Get in Touch
+                </motion.button>
+              </motion.div>
+
+
+
             </div>
-          ))}
+          </section>
+          <div className=" top-7/12 left-25  flex items-center justify-center  px-4 sm:px-6  font-inter">
+            <div className="max-w-7xl mx-auto w-full bg-white rounded-3xl  p-8 sm:p-12 relative overflow-hidden">
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {enquiryTypes.map((item, index) => (
+                  <div
+                    key={index}
+                    className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center text-center border border-gray-100 hover:shadow-lg transition-shadow duration-300"
+                  >
+                    <div className={`p-4 rounded-full ${item.iconBgColor} ${item.iconTextColor} mb-4`}>
+                      {item.icon}
+                    </div>
+                    <h3 className="text-xl font-bold text-gray-900 mb-2">
+                      {item.title}
+                    </h3>
+                    <p className="text-gray-600 text-base mb-6 flex-grow">
+                      {item.description}
+                    </p>
+                    <a
+                      href={item.buttonLink}
+                      className="inline-block bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold py-2.5 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                    >
+                      {item.buttonText}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <ContactUsPage />
+        <div className=" bg-white  text-gray-900 font-inter py-8 px-4 sm:px-6  flex flex-col items-center">
+          <div className="max-w-4xl mx-auto text-center mb-12">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-tight">
+              Lost? Don't worry, we're not <br />
+              <span className="text-red-600">hiding</span>! Find us here!
+            </h1>
+          </div>
+
+          <div className="flex lg:px-30  w-full rounded-lg  overflow-hidden">
+            <div className="w-full  min-h-[300px] bg-white  md:min-h-[450px]  flex items-center rounded-xl justify-center overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11487.327837261768!2d77.57889418196686!3d8.522276781053657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04692c8b82209f%3A0xa00060055fec4e1!2sRORIRI%20SOFTWARE%20SOLUTIONS%20PVT.LTD.!5e0!3m2!1sen!2sin!4v1735800795252!5m2!1sen!2sin"
+                width="100%"
+                height="100%"
+                style={{ backgroundColor: 'white' }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="RORIRI SOFTWARE SOLUTIONS PVT. LTD. Location"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-       
-    </div>
-<ContactUsPage/>
-   <div className=" bg-white  text-gray-900 font-inter py-8 px-4 sm:px-6  flex flex-col items-center">
-      <div className="max-w-4xl mx-auto text-center mb-12">
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-tight">
-          Lost? Don't worry, we're not <br />
-          <span className="text-red-600">hiding</span>! Find us here!
-        </h1>
-      </div>
-
-        <div className="flex lg:px-30  w-full rounded-lg  overflow-hidden">
-      <div className="w-full  min-h-[300px] bg-white  md:min-h-[450px]  flex items-center rounded-xl justify-center overflow-hidden">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11487.327837261768!2d77.57889418196686!3d8.522276781053657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04692c8b82209f%3A0xa00060055fec4e1!2sRORIRI%20SOFTWARE%20SOLUTIONS%20PVT.LTD.!5e0!3m2!1sen!2sin!4v1735800795252!5m2!1sen!2sin"
-          width="100%"
-          height="100%"
-          style={{  backgroundColor:'white' }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="RORIRI SOFTWARE SOLUTIONS PVT. LTD. Location"
-        ></iframe>
-      </div>
-    </div>
-    </div>
-    
-  </>
+    </>
   )
 }
 
--- src/pages/Services/components/IT_Consulting.jsx
+++ src/pages/Services/components/IT_Consulting.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { motion as Motion } from 'framer-motion';
-import { java1, dataanalyt1, testing1, node1, ui1, flutter1, angular1, react1 } from '../../../assets/services/service';
+import { java1, dataanalyt1, testing1, node1, ui1, flutter1, angular1, react1,itcon } from '../../../assets/services/service';
+import ServicesContact from './ServicesContact';
+import { useNavigate } from 'react-router-dom';
 
 const IT_Consulting = () => {
+
+    const navigate=useNavigate();
+    const gocontact = () =>
+    {
+        navigate("/contact")
+    }
     const sectionVariants = {
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } }
@@ -26,7 +34,7 @@ const IT_Consulting = () => {
                 variants={sectionVariants}
             >
                 <div className="absolute inset-0 z-0">
-                    <img src="https://placehold.co/1920x1080/1A202C/FFFFFF?text=IT+Consulting+Background" alt="IT Consulting Background" className="w-full h-full object-cover opacity-30" onError={(e) => { e.target.onerror = null; e.target.src = 'https://placehold.co/1920x1080/1A202C/FFFFFF?text=IT+Consulting+Background'; }} />
+                    <img src={itcon} alt="IT Consulting Background" className="w-full h-full object-cover opacity-30" onError={(e) => { e.target.onerror = null; e.target.src = {itcon}; }} />
                 </div>
                 <div className="container mx-auto px-4 flex flex-col items-center justify-center text-center relative z-10">
                     <p className="text-sm text-gray-400 uppercase tracking-wider mb-2 rounded-lg">IT Consulting</p>
@@ -46,19 +54,17 @@ const IT_Consulting = () => {
                     >
                         At Roriri Software Solutions, our IT consulting services offer invaluable support in navigating dynamic tech environments. Efficient IT consulting Solutions tailored to your needs. From CV Screening to onboarding, we find the right fit for your team. Streamline your hiring process with our expertise.
                     </Motion.p>
-                    <Motion.button
+                    <Motion.a href="#servicescontact"
                         className="cursor-pointer bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold py-3 px-8 rounded-full shadow-lg hover:bg-blue-600 transition duration-300"
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
                     >
                         Book a free consulting
-                    </Motion.button>
+                    </Motion.a>
                 </div>
             </Motion.section>
             <Motion.section
                 className="py-16 md:py-24 bg-white rounded-lg shadow-md mx-auto my-12 max-w-screen-xl px-4"
-                initial="hidden"
-                whileInView="visible"
                 viewport={{ once: true, amount: 0.3 }}
                 variants={sectionVariants}
             >
@@ -82,12 +88,12 @@ const IT_Consulting = () => {
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 At Roriri Software Solutions, we are providing the best ReactJS Developer for your IT needs. Need a React Developer? Look no further. We train to provide the best and most perfect React developer for you.
                             </p>
-                            <Motion.button
+                            {/* <Motion.button
                                 className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quot
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -101,13 +107,13 @@ const IT_Consulting = () => {
                             <h3 className="text-xl font-semibold text-gray-900 mb-3 rounded-lg">Angular Developer</h3>
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 Transform your business with Roriri Software Solutions' expert Angular developers. Our skilled professionals create dynamic, scalable web applications using Angular's powerful framework.    </p>
-                            <Motion.button
-                                className=" cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className=" cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -121,13 +127,13 @@ const IT_Consulting = () => {
                             <h3 className="text-xl font-semibold text-gray-900 mb-3 rounded-lg">Flutter Developer</h3>
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 Roriri Software Solutions brings you cutting-edge mobile app development with our expert Flutter developers. Build beautiful, natively compiled applications for mobile, web, and desktop from a single codebase.           </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -142,13 +148,13 @@ const IT_Consulting = () => {
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 At Roriri Software Solutions, our creative UI/UX designers craft exceptional digital experiences that captivate users and drive engagement. We combine aesthetic excellence with user-centered design principles to create intuitive interfaces. 
                              </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -162,13 +168,13 @@ const IT_Consulting = () => {
                             <h3 className="text-xl font-semibold text-gray-900 mb-3 rounded-lg">Node JS Developer</h3>
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 Roriri Software Solutions offers expert Node.js developers who build scalable, high-performance server-side applications. Our Node.js specialists leverage JavaScript's power to create efficient APIs, real-time applications, and microservices architectures.         </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -183,13 +189,13 @@ const IT_Consulting = () => {
                             <p className="text-gray-700 mb-6 rounded-lg">
                                 Unlock the power of your data with Roriri Software Solutions' expert data analysts. Our professionals transform raw data into actionable insights that drive strategic business decisions. Using advanced analytics tools and statistical methods. 
                             </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                         <Motion.div
                             className="bg-gray-100 p-8 rounded-xl shadow-lg text-left relative overflow-hidden group border border-gray-200"
@@ -204,13 +210,13 @@ const IT_Consulting = () => {
                             <p className="text-gray-700 mb-6 rounded-lg">
                          Ensure flawless software quality with Roriri Software Solutions' dedicated testing professionals. Our experienced software testers employ comprehensive testing methodologies including manual, automated, and performance testing to identify and eliminate bugs before deployment. 
                          </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
 
 
@@ -228,13 +234,13 @@ const IT_Consulting = () => {
 
                              Roriri Software Solutions provides enterprise-grade Java development services with our seasoned Java developers. Our experts build robust, scalable applications using Java's powerful ecosystem, including Spring Framework, microservices architecture, and cloud-native solutions.
                              </p>
-                            <Motion.button
-                                className="cursor-pointer inline-flex items-center text-blue-600 font-semibold hover:text-blue-800 transition duration-300"
+                            {/* <Motion.button
+                                className="cursor-pointer inline-flex items-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-semibold hover:text-blue-800 transition duration-300"
                                 whileHover={{ x: 5 }}
                             >
                                 Get Quote
                                 <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                            </Motion.button>
+                            </Motion.button> */}
                         </Motion.div>
                     </div>
                 </div>
@@ -255,7 +261,7 @@ const IT_Consulting = () => {
                         </p>
                     </div>
                     <div className="md:w-1/3 flex justify-center md:justify-end">
-                        <Motion.button
+                        <Motion.button onClick={gocontact}
                             className="cursor-pointer bg-white text-blue-800 font-semibold py-3 px-8 rounded-full shadow-lg hover:bg-gray-100 transition duration-300"
                             whileHover={{ scale: 1.05 }}
                             whileTap={{ scale: 0.95 }}
@@ -346,6 +352,7 @@ const IT_Consulting = () => {
                     </div>
                 </div>
             </Motion.section>
+            <ServicesContact/>
 
         </div>
     );
